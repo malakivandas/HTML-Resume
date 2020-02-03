e1d4e2ceb7e482d668a31c4520b8e0c320e70467
Malaki Vandas <malakivandas@gmail.com>
2020-02-02 18:04:41 -0800

    Created last_skillbar function

---
+++ sandbox/skillgrid/last_skillbar.js
@@ -0,0 +1,8 @@
+function last_skillbar () {
+
+    var last_skillbar_id = document.getElementById('skillgrid').childElementCount;
+    console.log(last_skillbar_id);
+
+    return last_skillbar_id;
+
+}
