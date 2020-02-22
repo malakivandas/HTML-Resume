d230e3228f149eacc313994717a6623884b4daf1
Malaki Vandas <malakivandas@gmail.com>
2020-02-21 18:57:14 -0800

    Mocked up some experience entries Inserted the skillgrid in the skills section

---
+++ REV2/js/add_skillbar.js
@@ -0,0 +1,39 @@
+function add_skillbar(num_skillpoints) {
+
+    //Generate skillbar ID
+    id_prefix = "skillbar_";
+    id_number = last_skillbar() + 1;
+    var skillbar_id = id_prefix.concat(id_number);
+    console.log(skillbar_id);
+
+    //Create div and set id
+    var skillbar = document.createElement("DIV");
+    skillbar.setAttribute("id", skillbar_id);
+    skillbar.setAttribute("class", "skillbar");
+
+    //Insert skillbar in skillgrid
+    document.getElementById("skillgrid").appendChild(skillbar);
+
+    //Insert input element
+    var skill_label = document.createElement("INPUT");
+    skill_label.setAttribute("class", "skill_label");
+    document.getElementById(skillbar_id).appendChild(skill_label);
+    
+
+    //Generate unfilled skillpoints
+    for(var idx = 0; idx < num_skillpoints; idx++) {
+
+        var skillpoint = document.createElement("BUTTON");
+        skillpoint.setAttribute("class", "unfilled_skillpoint");
+        skillpoint.setAttribute("id", "skillpoint_".concat(id_number, "_", idx+1));
+
+        console.log(skillpoint.id);
+
+        skillpoint.onclick = function() {
+            set_skillpoints(this.id, this.className)
+        }
+    
+        document.getElementById(skillbar_id).appendChild(skillpoint);
+    }
+
+}
---
+++ REV2/js/last_skillbar.js
@@ -0,0 +1,9 @@
+function last_skillbar() {
+
+    //Get the last skillbar's id number
+    var last_id_number = $("#skillgrid > div").length;
+    console.log(last_id_number);
+
+    return last_id_number;
+
+}
